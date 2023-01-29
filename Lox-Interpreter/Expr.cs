112162837cc236c0e8862e76f7295a179f2fb807
A. Hamdani <hamdaniryuuken@gmail.com>
2023-01-29 16:24:37 +0700

    Add AST generator

--- Lox-Interpreter/Expr.cs
+++ Lox-Interpreter/Expr.cs
@@ -1,18 +1,51 @@
-﻿namespace Lox_Interpreter;
+namespace Lox_Interpreter;
 
 public abstract class Expr
 {
     public class Binary : Expr
     {
-        private readonly Expr _left;
-        private readonly Token _operator;
-        private readonly Expr _right;
+
+        private readonly Expr left;
+        private readonly Token op;
+        private readonly Expr right;
 
         public Binary(Expr left, Token op, Expr right)
         {
-            _left = left;
-            _operator = op;
-            _right = right;
+            this.left = left;
+            this.op = op;
+            this.right = right;
+        }
+    }
+    public class Grouping : Expr
+    {
+
+        private readonly Expr expression;
+
+        public Grouping(Expr expression)
+        {
+            this.expression = expression;
+        }
+    }
+    public class Literal : Expr
+    {
+
+        private readonly Object value;
+
+        public Literal(Object value)
+        {
+            this.value = value;
+        }
+    }
+    public class Unary : Expr
+    {
+
+        private readonly Token op;
+        private readonly Expr right;
+
+        public Unary(Token op, Expr right)
+        {
+            this.op = op;
+            this.right = right;
         }
     }
 }
--- Lox-Tool/GenerateAst.cs
+++ Lox-Tool/GenerateAst.cs
@@ -6,20 +6,20 @@ public class GenerateAst
 {
     public static void Main(string[] args)
     {
-        if (args.Length != 1)
-        {
-            Console.Error.WriteLine("Usage: generate_ast <output directory>");
-            Environment.Exit(64);
-        }
+        // if (args.Length != 1)
+        // {
+        //     Console.Error.WriteLine("Usage: generate_ast <output directory>");
+        //     Environment.Exit(64);
+        // }
 
-        string outputDir = args[0];
+        string outputDir = @"C:\Users\Hamda\Documents\RiderProjects\Lox-Interpreter\Lox-Interpreter\";
         
         DefineAst(outputDir, "Expr", new List<string>
         {
-            "Binary   : Expr left, Token operator, Expr right",
+            "Binary   : Expr left, Token op, Expr right",
             "Grouping : Expr expression",
             "Literal  : Object value",
-            "Unary    : Token operator, Expr right"
+            "Unary    : Token op, Expr right"
         });
     }
 
@@ -30,9 +30,46 @@ public class GenerateAst
 
         writer.AppendLine("namespace Lox_Interpreter;");
         writer.AppendLine();
-        writer.AppendLine("public abstract class" + baseName);
+        writer.AppendLine("public abstract class " + baseName);
         writer.AppendLine("{");
-
+        
+        foreach (string type in types) {
+            string className = type.Split(":")[0].Trim();
+            string fields = type.Split(":")[1].Trim(); 
+            DefineType(writer, baseName, className, fields);
+        }
+        
         writer.AppendLine("}");
+        
+        File.WriteAllText(path, writer.ToString());
+    }
+
+    private static void DefineType(StringBuilder writer, string baseName, string className, string fieldList)
+    {
+        writer.AppendLine($"    public class {className} : {baseName}");
+        writer.AppendLine("    {");
+        
+        // Store parameters in fields.
+        string[] fields = fieldList.Split(", ");
+        
+        // Fields
+        writer.AppendLine();
+        foreach (var field in fields)
+        {
+            writer.AppendLine($"        private readonly {field};");
+        }
+        writer.AppendLine();
+
+        // Constructor
+        writer.AppendLine($"        public {className}({fieldList})");
+        writer.AppendLine("        {");
+        foreach (string field in fields)
+        {
+            string name = field.Split(" ")[1];
+            writer.AppendLine($"            this.{name} = {name};");
+        }
+        writer.AppendLine("        }");
+
+        writer.AppendLine("    }");
     }
 }
